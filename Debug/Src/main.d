e50f346c6cd17467eca44b1c97c3a6e928136167
efe <efebasol@gmail.com>
2023-06-07 02:37:44 +0300

    created SPI struct and CLK enable - disable macro def

--- Debug/Src/main.d
+++ Debug/Src/main.d
@@ -3,9 +3,11 @@ Src/main.o: ../Src/main.c \
  /Users/efebasol/STM32CubeIDE/workspace_1.12.0/driverDevelopment/Drivers/Inc/RCC.h \
  /Users/efebasol/STM32CubeIDE/workspace_1.12.0/driverDevelopment/Drivers/Inc/stm32f446xx.h \
  /Users/efebasol/STM32CubeIDE/workspace_1.12.0/driverDevelopment/Drivers/Inc/GPIO.h \
- /Users/efebasol/STM32CubeIDE/workspace_1.12.0/driverDevelopment/Drivers/Inc/EXTI.h
+ /Users/efebasol/STM32CubeIDE/workspace_1.12.0/driverDevelopment/Drivers/Inc/EXTI.h \
+ /Users/efebasol/STM32CubeIDE/workspace_1.12.0/driverDevelopment/Drivers/Inc/SPI.h
 /Users/efebasol/STM32CubeIDE/workspace_1.12.0/driverDevelopment/Drivers/Inc/stm32f446xx.h:
 /Users/efebasol/STM32CubeIDE/workspace_1.12.0/driverDevelopment/Drivers/Inc/RCC.h:
 /Users/efebasol/STM32CubeIDE/workspace_1.12.0/driverDevelopment/Drivers/Inc/stm32f446xx.h:
 /Users/efebasol/STM32CubeIDE/workspace_1.12.0/driverDevelopment/Drivers/Inc/GPIO.h:
 /Users/efebasol/STM32CubeIDE/workspace_1.12.0/driverDevelopment/Drivers/Inc/EXTI.h:
+/Users/efebasol/STM32CubeIDE/workspace_1.12.0/driverDevelopment/Drivers/Inc/SPI.h:
